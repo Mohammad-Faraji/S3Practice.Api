0209245664eb867faef76a2f85ebf5fd06fd9513
m.faraji9841@gmail.com <m.faraji9841@gmail.com>
2025-11-30 16:05:01 +0330

    Create FilesController

---
+++ S3Practice.Api/Controllers/FilesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace S3Practice.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+    }
+}
--- S3Practice.Api/Program.cs
+++ S3Practice.Api/Program.cs
@@ -1,3 +1,7 @@
+﻿using S3Practice.Api.Models.AppSetting;
+using S3Practice.Api.Service.Implement;
+using S3Practice.Api.Service.Interface;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// خواندن تنظیمات S3 از appsettings
+builder.Services.Configure<S3Configuration>(builder.Configuration.GetSection("S3Configuration"));
+
+// ثبت سرویس S3 در DI
+builder.Services.AddScoped<IFileStorageService, S3FileStorageService>();
 
 var app = builder.Build();
 
