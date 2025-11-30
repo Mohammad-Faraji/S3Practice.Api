4083e5d60193efe3de4d1808c0ebc6ec97d37b72
m.faraji9841@gmail.com <m.faraji9841@gmail.com>
2025-11-30 10:07:15 +0330

    Writing IFileStorageService

---
+++ S3Practice.Api/Service/Interface/IFileStorageService.cs
@@ -0,0 +1,9 @@
+﻿namespace S3Practice.Api.Service.Interface
+{
+    public interface IFileStorageService
+    {
+        Task<string> UploadAsync(IFormFile file, string name);
+        Task<Stream> DownloadAsync(string fileUrl, CancellationToken ct);
+        Task DeleteAsync(string fileUrl);
+    }
+}
