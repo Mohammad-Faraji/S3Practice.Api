9670a044b88210333bd4c6ef73191bc0e00778d3
m.faraji9841@gmail.com <m.faraji9841@gmail.com>
2025-11-30 09:56:19 +0330

    Writing the S3Configuration Class

---
+++ S3Practice.Api/Models/AppSetting/S3Configuration.cs
@@ -0,0 +1,13 @@
+﻿namespace S3Practice.Api.Models.AppSetting
+{
+    public class S3Configuration
+    {
+        public string AccessKey { get; set; } = string.Empty;
+        public string SecretKey { get; set; } = string.Empty;
+        public string Region { get; set; } = string.Empty;
+        public string ServiceUrl { get; set; } = string.Empty;
+        public string BucketName { get; set; } = string.Empty;
+       
+
+    }
+}
